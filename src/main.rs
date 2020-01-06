68dff7adce8eb021d8f6fb2ef7af23a040f61232
yon <valandil.yon@gmail.com>
2020-01-06 14:02:07 +0100

    actix_web simple http request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, HttpResponse, Responder};
+
+#[get("/{id}/{name}/")]
+async fn index(info: web::Path<(u32, String)>) -> impl Responder {
+    //format!("Hello {}! id:{}", info.1, info.0)
+    HttpResponse::Ok().body("Hello worl !")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
